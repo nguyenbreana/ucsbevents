fdbf75ed0108ffc4e0a4f7a5fb34617d29aefdec
Raven Simone <rayansu@ucsb.edu>
2019-01-26 15:46:59 -0800

    code

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function Nugget() {
 
 Nugget.prototype.checkSetup = function() {
   //Check to see if Firebase is set up. You shouldn't have to worry about this.
-  if(!window.firebase || (firebase.app instanceof Function) || !firebase.app().options) {
+  if(!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
     window.alert("There was an error importing the Firebase SDK.");
   }
 }
@@ -22,8 +22,7 @@ Nugget.prototype.initFirebase = function() {
   this.database = firebase.database();
   this.storage = firebase.storage();
 
-  this.listEvents;
-  //test change
+  this.addEvent();
 }
 
 Nugget.prototype.listEvents = function() {
@@ -31,6 +30,37 @@ Nugget.prototype.listEvents = function() {
   this.database.ref('events').on('value' , function(snapshot) {
     //Console.log logs things to the console. Good for debugging
     //snapshot.val() will contain the contents of the object you search for (events)
-    console.log(snapshot.val());
+    // console.log(snapshot.val());
+    var events = snapshot.val();
+
+    for(var i = 1; i <= events.length; i++) {
+      console.log(events[i]);
+    }
+
+
+
   })
 }
+
+Nugget.prototype.addEvent = function () {
+  this.database().ref('events').on('value', function(snapshot) {
+    var events = snapshot.val();
+
+    var newEvent = {
+      name: "SB Hacks",
+      time: "January",
+      location: "Corwin",
+      type: "Hackathon"
+    }
+
+    events.push(newEvent);
+
+    var updates = {};
+    updates['/events/'] = events;
+    this.database().ref().update(updates);
+
+    this.listEvents();
+  }.bind(this));
+
+
+};
